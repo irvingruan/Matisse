5353ada23f6069501b39e0a9a56a04b149f828cc
Irving Ruan <irvingruan@gmail.com>
2012-03-19 01:47:15 -0700

    Copying over .itc files to desktop dump folder working

--- Matisse.py
+++ Matisse.py
@@ -8,12 +8,18 @@
 import os
 import fnmatch
 import errno
+import mmap
+import contextlib
+import shutil
 
 pathToAlbumArtwork = '~/Music/iTunes/Album Artwork/Download/'
 pathToAlbumArtwork = os.path.expanduser(pathToAlbumArtwork)
 
-artworkDumpPath = '~/Desktop/Matisse_Dump'
+artworkDumpPath = '~/Desktop/Matisse_Dump/itc_dump/'
 artworkDumpPath = os.path.expanduser(artworkDumpPath)
+
+itcDumpPath = '~/Desktop/Matisse_Dump/itc_dump/'
+itcDumpPath = os.path.expanduser(itcDumpPath)
 
 """TO DO"""
 def usage():
@@ -40,17 +46,28 @@ def create_artwork_dump():
     
     try:
         os.makedirs(artworkDumpPath)
+        os.makedirs(itcDumpPath)
     except OSError, e:
         if e.errno != errno.EEXIST:
-            raise
+            raise Exception("Artwork dump directory already exists!")
+            
+# def copy_itc_files(itc_files):
+#     for itc_file in itc_files:
+#         copy(itc_file, artworkDumpPath)
             
 def parse_itunes_artwork(artwork_file):
+    
+    with open(artwork_file, 'r') as itc_file:
+        with contextlib.closing(mmap.mmap(itc_file.fileno(), 0,                access=mmap.ACCESS_READ)) as m:
+            print 'Bytes via 490 - 520 : ', m[490:521]
 
 def main():
 
     itc_list = retrieve_itc_files()
-
     create_artwork_dump()
+    
+    for itc_file in itc_list:
+        shutil.copy(itc_file, itcDumpPath)
 
 if __name__ == "__main__":
     main()
