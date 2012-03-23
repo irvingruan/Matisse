a1fd656dbbdef040d156ba56ac05f75fd688823a
Irving Ruan <irvingruan@gmail.com>
2012-03-23 01:39:20 -0700

    Prompt user for reconversion/recreation if they want if dump folder data already exists

--- Matisse.py
+++ Matisse.py
@@ -5,6 +5,7 @@
 
 # See README for instructions on how to use Matisse
 
+import sys
 import os
 import fnmatch
 import errno
@@ -13,6 +14,8 @@ import contextlib
 import shutil
 
 JPEG_SIGNATURE_OFFSET = 492
+artwork_item_count = 0
+artwork_name_prefix = "AlbumArtwork"
 
 artworkDumpPath = '~/Desktop/Matisse_Dump/'
 artworkDumpPath = os.path.expanduser(artworkDumpPath)
@@ -21,8 +24,8 @@ itcDumpPath = '~/Desktop/Matisse_Dump/itc_dump/'
 itcDumpPath = os.path.expanduser(itcDumpPath)
 
 """TO DO"""
-def usage():
-    raise None
+def print_usage():
+    print "Usage: ./Matisse.py\n"
 
 """TO DO"""
 def help():
@@ -62,8 +65,12 @@ def create_artwork_dump():
             raise Exception("Artwork dump folder already exists!")
             
 def create_jpeg_from_itc(artwork_file):
+    global artwork_item_count
+    global artwork_name_prefix
     
     try:
+        artwork_item_count += 1
+        
         itc_file_handle = open(artwork_file, "r+")
         byte_data = mmap.mmap(itc_file_handle.fileno(),0)
     
@@ -77,30 +84,60 @@ def create_jpeg_from_itc(artwork_file):
         byte_data = mmap.mmap(itc_file_handle.fileno(),0)
     
         jpeg_file = artwork_file.replace('.itc', '.jpeg')
+        
+        artwork_path_components = jpeg_file.split("/")
+        artwork_path_components[-1] = artwork_name_prefix + str(artwork_item_count) + ".jpeg"
+        jpeg_file = "/".join(artwork_path_components)
+        
         os.rename(artwork_file, jpeg_file)
     except:
-        sys.stderr.write("Could not convert %s to JPEG." % str(artwork_file))
-
+        sys.stderr.write("Error: could not convert %s to JPEG." % str(artwork_file))
+        
+def convert_proc():
+    
+    album_artwork_path = locate_album_artwork_path()
+    itc_list = retrieve_itc_files(album_artwork_path)
+    create_artwork_dump()
+        
+    # Copy over the .itc files so we don't modify iTunes version
+    # We simply want the album artwork
+    for itc_file in itc_list:
+        shutil.copy(itc_file, itcDumpPath)
+    
+    new_itc_files = os.listdir(itcDumpPath)
+    for itc_file in new_itc_files:
+        create_jpeg_from_itc(os.path.join(itcDumpPath, itc_file))
+    
+    print "Finished converting iTunes album artwork files to JPEGs. They are located at " + artworkDumpPath
+        
 def main():
+    
+    if len(sys.argv) > 1:
+        print_usage()
+        sys.exit(-1)
+    
+    global artwork_item_count
+    artwork_item_count = 0
 
     # If we already have the JPEGs, no need to convert it again
     if not(os.path.exists(artworkDumpPath) and os.listdir(artworkDumpPath)):
         
-        album_artwork_path = locate_album_artwork_path()
-        itc_list = retrieve_itc_files(album_artwork_path)
-        create_artwork_dump()
-            
-        # Copy over the .itc files so we don't modify iTunes version
-        # We simply want the album artwork
-        for itc_file in itc_list:
-            shutil.copy(itc_file, itcDumpPath)
-        
-        new_itc_files = os.listdir(itcDumpPath)
-        for itc_file in new_itc_files:
-            create_jpeg_from_itc(os.path.join(itcDumpPath, itc_file))
+        convert_proc()
         
     else:
-        print "Album artwork dump folder already exists with data."
+        sys.stderr.write("Album artwork dump folder already exists. Recreate anyway? (y/n):")
+        key = 0
+        try:
+            key = sys.stdin.read(1)
+        except KeyboardInterrupt:
+            key = 0
+            
+        if key == 'y':
+            convert_proc()
+        elif key == 'n':
+            sys.stderr.write("\nView your artwork at " + artworkDumpPath)
+        elif key == 0:
+            sys.stderr.write("\nError: keyboard interrupted.")
 
 
 if __name__ == "__main__":
