97be75b21beedce88ec85ece144bad616ca122e1
Irving Ruan <irvingruan@gmail.com>
2012-03-23 20:46:38 -0700

    Added function docstrings

--- Matisse.py
+++ Matisse.py
@@ -28,10 +28,12 @@ def print_usage():
     print "Usage: ./Matisse.py\n"
 
 def help():
-    raise None
+    print "For help, see README.\n"
     
 def locate_album_artwork_path():
+    """Finds local iTunes Album Artwork location"""
     
+    # Only recursively search down Music directory
     album_artwork_path = os.path.expanduser("~/Music")
     
     for root, subFolders, files in os.walk(album_artwork_path):
@@ -41,8 +43,12 @@ def locate_album_artwork_path():
                 path_components = os.path.join(root, sf).split("/")
                 if path_components[-2].lower() == "album artwork":
                     return os.path.join(root, sf)
+                    
+    sys.stderr.write("Unable to find iTunes Album Artwork director. Make sure your album artwork is located in your iTunes music directory as \"Album Artwork.\"\n")
+    sys.exit(-1)
 				
 def retrieve_itc_files(album_artwork_path):
+    """Grabs the list of .itc files in Download folder"""
     
     itc_list = []
     pattern = '*.itc'
@@ -55,14 +61,18 @@ def retrieve_itc_files(album_artwork_path):
     return itc_list
     
 def create_artwork_dump():
+    """Creates the artwork dump directory of HTML/artwork output"""
     
     try:
         os.makedirs(artworkDumpPath+'/artwork')
     except OSError, e:
         if e.errno != errno.EEXIST:
             raise Exception("Artwork dump folder already exists!")
+            sys.exit(-1)
             
 def create_jpeg_from_itc(artwork_file):
+    """Parses out JPEG from .itc files"""
+    
     global artwork_item_count
     global artwork_name_prefix
     
@@ -75,6 +85,7 @@ def create_jpeg_from_itc(artwork_file):
         file_size = len(byte_data)
         new_size = file_size - JPEG_SIGNATURE_OFFSET
 
+        # Extract out ITC metadata info that we don't need for now
         byte_data.move(0, JPEG_SIGNATURE_OFFSET, file_size - JPEG_SIGNATURE_OFFSET)
         byte_data.flush()
         byte_data.close()
@@ -90,8 +101,11 @@ def create_jpeg_from_itc(artwork_file):
         os.rename(artwork_file, jpeg_file)
     except:
         sys.stderr.write("Error: could not convert %s to JPEG." % str(artwork_file))
+        sys.exit(-1)
         
 def generate_html():
+    """Generate index.html for webpage with enclosed JPEG img src locations"""
+    
     try:
         artwork_jpegs = os.listdir(artworkDumpPath + "/artwork")
         
@@ -100,7 +114,7 @@ def generate_html():
         html_output.write(mhtml.body_start)
         
         item = 0
-        for i in range(0, len(artwork_jpegs) - 200):
+        for i in range(0, len(artwork_jpegs)):
             html_output.write("\t\t\t<div class=\"item\">\n")
             html_output.write("\t\t\t\t<img class=\"content\" src=\"artwork/AlbumArtwork" + str(i+1) + ".jpeg\"/>\n")
             html_output.write("\t\t\t</div>\n")
@@ -113,6 +127,8 @@ def generate_html():
         sys.exit(-1)
         
 def deploy_matisse():
+    """Copy over required HTML/JS/CSS files to the publish-ready directory"""
+    
     try:
         # Copy over the .html, .js, and .css files
         shutil.move(os.getcwd() + "/index.html", artworkDumpPath)
@@ -121,6 +137,7 @@ def deploy_matisse():
         rv = subprocess.Popen('cp -rf ' + os.getcwd() + '/publish/. ' + artworkDumpPath, shell=True)
         rv.wait()
         
+        # Fire up Safari to see the result
         rv = subprocess.Popen('open /Applications/Safari.app ' + artworkDumpPath + '/index.html', shell=True)
         rv.wait()
         
@@ -129,6 +146,8 @@ def deploy_matisse():
         sys.exit(-1)
         
 def convert_proc():
+    """Starts the process for obtaining .itc files and converting them"""
+    
     global local_url_prefix
     
     album_artwork_path = locate_album_artwork_path()
@@ -144,10 +163,7 @@ def convert_proc():
     new_itc_files = os.listdir(artwork_path)
     for itc_file in new_itc_files:
         create_jpeg_from_itc(os.path.join(artwork_path, itc_file))
-    
-    print "Finished converting iTunes album artwork files to JPEGs.\nThey are located at " + artwork_path
-    
-
+        
 def main():
     
     if len(sys.argv) > 1:
@@ -180,6 +196,9 @@ def main():
             sys.stderr.write("\nView your artwork at " + artworkDumpPath)
         elif key == 0:
             sys.stderr.write("\nError: keyboard interrupted.")
+            sys.exit(-1)
+            
+    sys.exit(0)
 
 
 if __name__ == "__main__":
